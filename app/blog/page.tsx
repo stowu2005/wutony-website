f0938cf328cde476e8cce5de4663fc275878a9ae
stowu2005 <stowu2005@gmail.com>
2024-08-27 23:17:34 -0700

    Updated URLs

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@ import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
   title: 'Blog',
-  description: 'Read my blog.',
+  description: 'Read Tony\'s blog.',
 }
 
 export default function Page() {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'https://wutony.com'
+export const baseUrl = 'https://www.wutony.com'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
