72ed364ac14d04cf2f08a59ba50a40aee95e4a08
stowu2005 <stowu2005@gmail.com>
2025-03-31 15:54:17 -0700

    Updated Home page

--- app/page.tsx
+++ app/page.tsx
@@ -20,45 +20,79 @@ export default function Page() {
           rel="noopener noreferrer"
           target="_blank"
           href="https://www.cs.washington.edu/"
-        >Computer Science</a>. I am interested in programming languages, natural language processing, and geography.
+        >Computer Science</a>. I am interested in natural language processing, data visualization, and geography.
       </p>
-      <p className="mb-8">Some courses I've taken include <a
-      className="transition-all hover:text-neutral-500 dark:hover:text-blue-300 font-bold dark:font-normal dark:hover:underline dark:text-blue-600"
-      rel="noopener noreferrer"
-      target="_blank"
-      href="https://courses.cs.washington.edu/courses/cse332/24su/">
-      Data Structures and Parallelism</a>, <a
-      className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
+      <p className="mb-4">The classes I took last quarter were <a
+        className="transition-all hover:text-neutral-500 dark:hover:text-blue-300 font-bold dark:font-normal dark:hover:underline dark:text-blue-600"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://courses.cs.washington.edu/courses/cse447/25wi/">
+        Natural Language Processing</a>, <a
+          className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
       dark:hover:underline dark:text-orange-500"
-      rel="noopener noreferrer"
-      target="_blank"
-      href="https://courses.cs.washington.edu/courses/cse331/24sp/">
-      Software Design & Implementation</a>, <a
-      className="transition-all hover:text-neutral-500 dark:hover:text-red-300 font-bold dark:font-normal
-      dark:hover:underline dark:text-red-500"
-      rel="noopener noreferrer"
-      target="_blank"
-      href="https://courses.cs.washington.edu/courses/cse333/24su/">
-      Systems Programming</a>, and <a
-      className="transition-all hover:text-neutral-500 dark:hover:text-emerald-300 font-bold dark:font-normal
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://courses.cs.washington.edu/courses/cse451/25wi/">
+          Introduction to Operating Systems</a>, and <a
+            className="transition-all hover:text-neutral-500 dark:hover:text-emerald-300 font-bold dark:font-normal
       dark:hover:underline dark:text-emerald-600"
-      rel="noopener noreferrer"
-      target="_blank"
-      href="https://courses.cs.washington.edu/courses/cse312/24sp/calendar/calendar.html">
-      Foundations of Computing II
-      </a>.</p>
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://courses.cs.washington.edu/courses/cse421/25wi/">
+          Introduction to Algorithms
+        </a>.</p>
+      <p className="mb-8"> Some other classes I have taken include <a
+        className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
+      dark:hover:underline dark:text-orange-500"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://courses.cs.washington.edu/courses/cse442/24au/">
+        Data Visualization</a>, <a
+          className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
+      dark:hover:underline dark:text-orange-500"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://courses.cs.washington.edu/courses/cse446/24au/">
+          Machine Learning</a>, <a
+          className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
+      dark:hover:underline dark:text-orange-500"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://courses.cs.washington.edu/courses/cse403/24au/">
+          Software Engineering</a>, <a
+          className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
+      dark:hover:underline dark:text-orange-500"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://courses.cs.washington.edu/courses/cse473/24au/">
+          Introduction to Artificial Intelligence</a>, and <a
+          className="transition-all hover:text-neutral-500 dark:hover:text-orange-200 font-bold dark:font-normal
+      dark:hover:underline dark:text-orange-500"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://courses.cs.washington.edu/courses/cse333/24su/">
+          Systems Programming</a>.
+      </p>
       <p className="mb-2">To learn more about me, go to the <Link
         className="transition-all hover:text-neutral-400 dark:hover:text-neutral-400 text-neutral-800
         dark:text-neutral-50"
         href="/about">
-          <b>about</b>
-        </Link> page.</p>
+        <b>about</b>
+      </Link> page.</p>
       <p className="mb-2">To look at some of the projects I've made, go to the <Link
         className="transition-all hover:text-neutral-400 dark:hover:text-neutral-400 text-neutral-800
         dark:text-neutral-50"
         href="/projects">
-          <b>projects</b>
-        </Link> page.</p>
+        <b>projects</b>
+      </Link> page.</p>
+      <p className="mb-2">To see the source code for this website, click <a
+        className="transition-all hover:text-neutral-400 dark:hover:text-neutral-400 text-neutral-800
+        dark:text-neutral-50"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/stowu2005/wutony-website">
+        <b>here</b>
+      </a>.</p>
     </section>
   )
 }
